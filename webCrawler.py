ec00a01460673d8ecab39d95e922197881ae1fd2
RamiroCruz <27798751+ramirocruz@users.noreply.github.com>
2025-11-18 00:18:06 +0530

    Update webCrawler.py

--- webCrawler.py
+++ webCrawler.py
@@ -1,29 +1,33 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://1x-probet.com/en"  # Replace with the website you are studying
-
-response = requests.get(url)
-response.raise_for_status()  # Fails if the request is bad
-
-soup = BeautifulSoup(response.text, "html.parser")
+baseUrl = "https://1x-probet.com/en"
+esportsUrl = "https://1x-probet.com/en/live/esports"
 
 
-# Find the specific div group
-target_div = soup.find("div", class_="betting-main-dashboard")
+def get_title_from_url(url):
+    print("Getting data from : {url}\n".format(url=url))
+    response = requests.get(url)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, "html.parser")
+    # Find the specific div group
+    target_div = soup.find("div", class_="betting-main-dashboard")
+    if not target_div:
+        print("No div with class='betting-main-dashboard' found")
+    else:
+        # Find all li elements inside the div
+        li_elements = target_div.find_all("li")
 
-if not target_div:
-    print("No div with class='betting-main-dashboard' found")
-else:
-    # Find all li elements inside the div
-    li_elements = target_div.find_all("li")
+        results = []
+        for li in li_elements:
+            span = li.find("span", class_="ui-caption")
+            if span:
+                results.append(span.get_text(strip=True))
 
-    results = []
-    for li in li_elements:
-        span = li.find("span")
-        if span:
-            results.append(span.get_text(strip=True))
+        print("Extracted values:")
+        for idx, r in enumerate(results):
+            print("{idx}: {r}".format(idx=idx, r=r))
 
-    print("Extracted values:")
-    for r in results:
-        print(r)
+
+get_title_from_url(baseUrl)
+get_title_from_url(esportsUrl)
